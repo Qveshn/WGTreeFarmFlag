6d79285444ffdf08b3b3dd72c0221dfa6f7c2001
src-dev <spencer.ryan.c@gmail.com>
2016-01-04 16:52:28 -0800

    Removed redundancy

--- src/net/src_dev/wgtreefarmflag/MushroomIntervalGrower.java
+++ src/net/src_dev/wgtreefarmflag/MushroomIntervalGrower.java
@@ -27,21 +27,19 @@ public class MushroomIntervalGrower implements Runnable{
 			for(Block b:entry.getValue()){
 				Location loc = b.getLocation();
 				World w = loc.getWorld();
-				if(w.getBlockAt(loc).getType() == Material.RED_MUSHROOM || w.getBlockAt(loc).getType() == Material.BROWN_MUSHROOM){
-					Material type = b.getType();
-					TreeType treeType = null;
-					if(type == Material.BROWN_MUSHROOM) treeType = TreeType.BROWN_MUSHROOM;
-					else if(type == Material.RED_MUSHROOM) treeType = TreeType.RED_MUSHROOM;
-					int r = random.nextInt(99) + 1;
-					boolean grew;
-					if(r <= growthChance){
-						b.setType(Material.AIR);
-						grew = w.generateTree(loc, treeType);	
-						if(!grew){
-							b.setType(type);
-						}
-						
+				Material type = b.getType();
+				TreeType treeType = null;
+				if(type == Material.BROWN_MUSHROOM) treeType = TreeType.BROWN_MUSHROOM;
+				else if(type == Material.RED_MUSHROOM) treeType = TreeType.RED_MUSHROOM;
+				int r = random.nextInt(99) + 1;
+				boolean grew;
+				if(r <= growthChance){
+					b.setType(Material.AIR);
+					grew = w.generateTree(loc, treeType);	
+					if(!grew){
+						b.setType(type);
 					}
+					
 				}
 			}
 		}
